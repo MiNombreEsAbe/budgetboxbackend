1f153f67293e9d919315f8b6f9d16e0b413cb26b
morenoa2 <morenoa2@berea.edu>
2022-03-24 15:48:58 -0400

    Moved requirements.txt

--- backend/settings.py
+++ backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-q6o(&do23$ex%q=vrnepk@i-!@v=geg_i3yb=2=5=fuy+3cg3k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
